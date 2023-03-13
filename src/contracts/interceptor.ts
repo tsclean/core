30ebed8d5b498123a1ba651bc6b6b77be59efec3
John Piedrahita <citymanager02@gmail.com>
2023-03-13 01:47:47 +0100

    refactor: change promesa for observable in interceptor

--- src/contracts/interceptor.ts
+++ src/contracts/interceptor.ts
@@ -2,12 +2,12 @@ import {Observable} from "rxjs";
 import {ExecutionContextInterface} from './execution-context';
 
 export interface CallHandlerInterface<T = any> {
-    handle(): Promise<T> | Observable<T>;
+    handle(): Observable<T>;
 }
 
 export interface InterceptorInterface<T = any, R = any> {
     intercept(
         context: ExecutionContextInterface,
         next: CallHandlerInterface<T>
-    ): Promise<R> | Promise<Promise<R>> | Observable<R> | Promise<Observable<R>>;
+    ): Observable<R> | Promise<Observable<R>>;
 }
